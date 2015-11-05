a810b2d99668783258adc50d8c50a409dcbee893
monsteronfire <monsteronfire@gmail.com>
2015-11-05 18:02:25 +0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name switchGridApp
+ * @description
+ * # switchGridApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('switchGridApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name switchGridApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the switchGridApp
+ */
+angular.module('switchGridApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
