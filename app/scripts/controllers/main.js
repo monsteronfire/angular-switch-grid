4031dc53e7f7fc5eab8d1176b76765b0fdd9cc47
monsteronfire <monsteronfire@gmail.com>
2015-11-06 12:07:53 +0800

    adds controller code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,32 @@
  * # MainCtrl
  * Controller of the switchGridApp
  */
-angular.module('switchGridApp')
-  .controller('MainCtrl', function () {
+angular.module('switchGridApp', ['ngResource'])
+  .factory('instagram', function($resource) {
+    return {
+      fetchPopular: function(callback) {
+        var api = $resource('https://api.instagram.com/v1/media/popular?client_id=:client_id&callback=JSON_CALLBACK',{client_id: '8d0c4adf568146c7968559a9a7ee4d2f'},{fetch:{method:'JSONP'}
+        });
+
+        api.fetch(function(response) {
+          callback(response.data);
+        });
+      }
+    }
+  })
+  .controller('MainCtrl', function ($scope, instagram) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.layout = 'grid';
+    $scope.pics = [];
+
+    $scope.switchGrid = function() {
+      instagram.fetchPopular(function(data) {
+        $scope.pics = data;
+      });
+    };
   });
